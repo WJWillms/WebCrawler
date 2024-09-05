d6b465bd671b99b2f80fd9c35845273dd25a947f
Wade Willms <wade@otisinstruments.com>
2024-09-05 10:43:59 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/WJWillms/WebCrawler
+
+go 1.23.0
---
+++ normalize_url.go
@@ -0,0 +1,7 @@
+package main
+
+// normalizeURL is a stub function that will be implemented later.
+func normalizeURL(url string) (string, error) {
+	// TODO: Implement the function to normalize the URL
+	return "", nil
+}
